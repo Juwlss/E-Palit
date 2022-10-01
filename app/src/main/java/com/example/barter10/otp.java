e52cd7c457b3fbd37239ec6dcd5bd7ee22d46cef
Kyu18 <keannulacambra18@gmail.com>
2022-10-01 17:46:25 +0800

    otp

---
+++ app/src/main/java/com/example/barter10/otp.java
@@ -0,0 +1,48 @@
+package com.example.barter10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+import com.chaos.view.PinView;
+
+public class otp extends AppCompatActivity {
+
+    PinView pinView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+        pinView=findViewById(R.id.pin1);
+
+        pinView.requestFocus();
+        InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,inputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        pinView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().length()==4){
+                    Toast.makeText(getApplicationContext(), "Working", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+}
