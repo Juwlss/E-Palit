71b8c11f636e67da9d37b601defe4075e2ac1c52
Juwlss <98637159+Juwlss@users.noreply.github.com>
2022-11-10 11:17:14 +0800

    fixing

--- app/src/main/java/com/example/barter10/Model/User.java
+++ app/src/main/java/com/example/barter10/Model/User.java
@@ -3,64 +3,60 @@ package com.example.barter10.Model;
 public class User {
     String userID, fullname, profilepic, username, userpassword, userPhone;
 
-
-    public User(){
-
+    public User() {
     }
 
     public User(String userID, String fullname, String profilepic, String username, String userpassword, String userPhone) {
-        this.username = userID;
+        this.userID = userID;
         this.fullname = fullname;
         this.profilepic = profilepic;
         this.username = username;
         this.userpassword = userpassword;
         this.userPhone = userPhone;
-
     }
 
     public String getUserID() {
         return userID;
     }
 
-    public String getFullname() {
-        return fullname;
-    }
-
-    public String getProfilepic() {
-        return profilepic;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-
-    public String getUserpassword() {
-        return userpassword;
-    }
-
-    public String getUserPhone() {
-        return userPhone;
-    }
-
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    public String getFullname() {
+        return fullname;
     }
 
     public void setFullname(String fullname) {
         this.fullname = fullname;
     }
 
+    public String getProfilepic() {
+        return profilepic;
+    }
+
     public void setProfilepic(String profilepic) {
         this.profilepic = profilepic;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getUserpassword() {
+        return userpassword;
+    }
+
     public void setUserpassword(String userpassword) {
         this.userpassword = userpassword;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
     }
 
     public void setUserPhone(String userPhone) {
